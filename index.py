9c4bc35cbb86707688ef4fea8b74901a60fa2202
s1131227 <machongen@gmail.com>
2026-03-28 23:58:58 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,15 +1,22 @@
-import math
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from datetime import datetime
-app = Flask(__name__)
+
+app = Flask(_name_)
 
 @app.route("/")
 def index():
-    link = "<h1>資訊管理導論(馬崇恩)</h1>"
+    link = "<h1>歡迎進入馬崇恩的網站</h1>"
     link += "<a href=/mis>課程</a><hr>"
     link += "<a href=/today>現在日期時間</a><hr>"
-    link += "<a href=/welcome?u=崇恩&=靜宜資管&=資訊管理導論>Get傳</a><hr>"
     link += "<a href=/me>關於我</a><hr>"
+    link += "<a href=/welcome?u=崇恩&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
+    link += "<a href=/calculate>次方與根號計算</a><hr>"
+    return link
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
+
 @app.route("/today")
 def today():
     now = datetime.now()
@@ -17,7 +24,7 @@ def today():
 
 @app.route("/me")
 def me():
-    return render_template("mis2026b.html")
+    return render_template("mis2026 B.html")
 
 @app.route("/welcome", methods=["GET"])
 def welcome():
